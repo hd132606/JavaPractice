6dd0ae82da917f4d2bf3765313673e1f28e65bbe
xeroskim <xeroskim@xeros>
2015-09-03 11:17:36 +0900

    실습과제 3번째

---
+++ JavaPractice/src/org/dimigo/abstractclass/IPhone.java
@@ -0,0 +1,14 @@
+package org.dimigo.abstractclass;
+
+public class IPhone extends SmartPhone {
+	public IPhone() { }
+	public IPhone(String model, String company, int price){
+		super(model, company, price);
+	}
+	public void pay(){
+		super.pay();
+	}
+	public void useAirDrop(){
+		System.out.println("AirDrop 기능을 사용합니다.");
+	}
+}
---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,19 @@
+package org.dimigo.abstractclass;
+
+public class SmartPhoneTest {
+
+	public static void main(String[] args) {
+		SmartPhone[]a = {
+				new IPhone("IPhone6", "애플", 700000),
+				new Galaxy("갤럭시 S6", "삼성", 650000)
+		};
+		for(SmartPhone phone : a){
+			System.out.println(phone);
+			phone.turnOn();
+			phone.pay();
+			phone.useSpecialFunction(phone);
+			phone.turnOff();
+			System.out.println("\n");
+		}
+	}
+}
